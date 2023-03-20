1e73d7a9ca331d238e269cd3f6777f5bd4490280
Benjamin Dehli <superelg@gmail.com>
2023-03-20 15:21:27 +0100

    Add title, default value and null check

--- Internprosjekt_frontend_docker_compose/src/App.js
+++ Internprosjekt_frontend_docker_compose/src/App.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 import Product from "./components/partials/Product";
 
 function App() {
-  const [productsData, setProductsData] = useState("Helge");
+    const [productsData, setProductsData] = useState({
+        total: 0,
+        products: []
+    });
 
   async function getProductsData() {
     const response = await fetch("https://dummyjson.com/products");
@@ -11,23 +14,17 @@ function App() {
   }
 
   function renderProducts(productsData) {
-    return (
-      productsData?.products?.length && 
-      productsData.products.map((product) => {
-        return (
-          <Product key={product.id} product={product} />
-        )
+        return productsData?.products?.length
+            ? productsData.products.map((product) => {
+                  return <Product key={product.id} product={product} />;
       })
-    )
+            : null;
   }
-
-
 
   return (
     <div>
-      <button onClick={getProductsData}>
-        Get products
-      </button>
+            <h1>Products</h1>
+            <button onClick={getProductsData}>Get products</button>
       {renderProducts(productsData)}
     </div>
   );
